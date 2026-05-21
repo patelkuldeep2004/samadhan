da38d18b6776814bc912d68a220e4f8d89eb112c
kuldeep <patelkuldeep2004@gmail.com>
2026-05-21 23:57:23 +0530

    update DB connection

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,10 +1,17 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const sequelize = new Sequelize('samadhan_db', 'root', '11223344', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  }
+);
 
 export default sequelize;
